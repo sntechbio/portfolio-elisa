1b4afae15d9992b04c8491f9453d0f7021295cb4
¨leandro-lcsistemas¨ <leandro.nascimento@lcsistemas.com.br>
2024-10-24 22:18:36 -0300

    iniciando componentes dos packs

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import PackOne from './pack1/index.jsx'
+import Pack from './pack1/index.jsx'
+
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <PackOne />
+    <Pack />
   </StrictMode>,
 )
